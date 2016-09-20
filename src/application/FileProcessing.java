372cf54f006cb2ff53649ab879bb3fe98c7b38fc
blaskowski94 <laskowsr@augsburg.edu>
2016-09-20 12:39:42 -0500

    Changed formatting to increase readability.

--- src/application/FileProcessing.java
+++ src/application/FileProcessing.java
@@ -6,9 +6,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * The FileProcessing class can be used with the GUI class or independently. An
- * instance of FileProcessing takes in a text file and allows you to determine
- * information about the file. There are methods to count the number of words,
+ * The FileProcessing class can be used with the GUI class or independently. An instance of FileProcessing takes in a
+ * text file and allows you to determine information about the file. There are methods to count the number of words,
  * count the number of different words and the count of each individual word.
  * 
  * @author Bob Laskowski
@@ -16,29 +15,17 @@ import java.util.Scanner;
  */
 public class FileProcessing {
 	
-	private File						file;			// File object to hold
-														// the text file
-	private HashMap<String, Integer>	wordMap;		// HashMap to determine
-														// count by word
-	private boolean						fileProcessed;	// True if processFile()
-														// method has been
-														// called on instance,
-														// false otherwise
-	private boolean						fileLoaded;		// True if file is set
-														// to a valid .txt file
-														// in constructor, false
-														// otherwise
-	private int							totalWordCount;	// Total number of words
-														// in the text document
+	private File file; // File object to hold the text file
+	private HashMap<String, Integer> wordMap; // HashMap to determine count by word
+	private boolean fileProcessed; // True if processFile() method has been called on instance, false otherwise
+	private boolean fileLoaded; // True if file is set to a valid .txt file in constructor, false otherwise
+	private int totalWordCount; // Total number of words in the text document
 	
 	/**
-	 * Creates a FileProcessing object. A new object should be created for every
-	 * file you wish to process. Constructor checks file passed in to make sure
-	 * it exists, is a file and not a directory and that it has a .txt extension
-	 * at the end. If all these conditions are not met, an error message is
-	 * printed and fileLoaded is set to false. If the conditions are met, file
-	 * is set to the file passed in. Also declares the HashMap and fileProcessed
-	 * boolean.
+	 * Creates a FileProcessing object. A new object should be created for every file you wish to process. Constructor
+	 * checks file passed in to make sure it exists, is a file and not a directory and that it has a .txt extension at
+	 * the end. If all these conditions are not met, an error message is printed and fileLoaded is set to false. If the
+	 * conditions are met, file is set to the file passed in. Also declares the HashMap and fileProcessed boolean.
 	 * 
 	 * @param f
 	 *            A .txt file with words
@@ -46,7 +33,7 @@ public class FileProcessing {
 	public FileProcessing(File f) {
 		if (!f.exists() || !f.isFile()
 				|| !f.getName().substring(f.getName().lastIndexOf('.'), f.getName().length()).equals(".txt")) {
-			//System.out.println("Error loading file: file must be a .txt file");
+			// System.out.println("Error loading file: file must be a .txt file");
 			fileLoaded = false;
 		} else {
 			file = f;
@@ -58,13 +45,11 @@ public class FileProcessing {
 	}
 	
 	/**
-	 * This method parses the text file using a scanner. It removes all
-	 * punctuation (except apostrophes), spaces and converts everything to lower
-	 * case. It populates the object's hash map with the words as keys and the
-	 * count of the words as the values.
+	 * This method parses the text file using a scanner. It removes all punctuation (except apostrophes), spaces and
+	 * converts everything to lower case. It populates the object's hash map with the words as keys and the count of the
+	 * words as the values.
 	 * 
-	 * @return A HashMap with all words as keys and number of occurrence as
-	 *         values
+	 * @return A HashMap with all words as keys and number of occurrence as values
 	 * @throws FileNotFoundException
 	 *             Thrown if the Scanner has a problem loading the file
 	 */
@@ -109,9 +94,8 @@ public class FileProcessing {
 	}
 	
 	/**
-	 * Returns the total number of words in the text file. The total is counted
-	 * during the processFile method. Method returns 0 by default if a file has
-	 * not been loaded. If the processFile method has not yet been called,
+	 * Returns the total number of words in the text file. The total is counted during the processFile method. Method
+	 * returns 0 by default if a file has not been loaded. If the processFile method has not yet been called,
 	 * processFile is called.
 	 * 
 	 * @return total number of words in the text document
@@ -127,10 +111,9 @@ public class FileProcessing {
 	}
 	
 	/**
-	 * Counts the total number of different words in the text document. This is
-	 * determined by the number of keys in the hash map. The method first checks
-	 * to see if the file was loaded successfully and if processFile has been
-	 * called and calls it if it has not.
+	 * Counts the total number of different words in the text document. This is determined by the number of keys in the
+	 * hash map. The method first checks to see if the file was loaded successfully and if processFile has been called
+	 * and calls it if it has not.
 	 * 
 	 * @return the size of the hash map, which is the number of different words
 	 */
@@ -146,13 +129,11 @@ public class FileProcessing {
 	}
 	
 	/**
-	 * This method first checks to see if the file was loaded correctly and if
-	 * processFile has been called and calls process file if it has not. It
-	 * returns the hash map populated in processFile. This contains the words as
-	 * keys and the values as the number of occurrences.
+	 * This method first checks to see if the file was loaded correctly and if processFile has been called and calls
+	 * process file if it has not. It returns the hash map populated in processFile. This contains the words as keys and
+	 * the values as the number of occurrences.
 	 * 
-	 * @return A hash map of all words and number of occurrences in the .txt
-	 *         document
+	 * @return A hash map of all words and number of occurrences in the .txt document
 	 */
 	public HashMap<String, Integer> countByWord() {
 		if (fileLoaded) {
